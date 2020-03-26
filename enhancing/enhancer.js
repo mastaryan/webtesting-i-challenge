c4a579da79c7debcab482ab8e89e52ae52c5b283
mastaryan <sizemore.ryan@gmail.com>
2020-03-25 21:05:30 -0400

    success method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,15 +2,32 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+      return {...item, enhancement: item.enhancement + 1 };
+  } else if (item.enhancement === 20) {
+      return {...item }
+  } else {
+      item.enhancement += 1;
+      return {...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15 && item.enhancement <= 16) {
+    return { ...item, durability: item.durability - 10 };
+  } else if (item.enhancement > 16) {
+    return {
+      ...item,
+      enhancement: item.enhancement - 1,
+      durability: item.durability - 10
+    };
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -21,3 +21,34 @@ describe("enhancer.js", () => {
     });
   });
 });
+
+describe("enhancer.succeed()", () => {
+  it("increases an item's enhancement by 1", () => {
+      const expectedItem = {
+          name: "item",
+          enhancement: 20,
+          durability: 87
+      }
+      const inputItem = {
+          name: "item",
+          enhancement: 19,
+          durability: 87
+      }
+      const outputItem = enhancer.succeed(inputItem)
+      expect(outputItem).toStrictEqual(expectedItem)
+  })
+  it("does not increase an item's enhancement level if it is already 20", () => {
+      const expectedItem = {
+          name: "item",
+          enhancement: 20,
+          durability: 100
+      }
+      const inputItem = {
+          name: "item",
+          enhancement: 20,
+          durability: 100
+      }
+      const outputItem = enhancer.succeed(inputItem)
+      expect(outputItem).toStrictEqual(expectedItem)
+  })
+})
