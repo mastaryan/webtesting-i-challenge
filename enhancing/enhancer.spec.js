f05276bd9ef4742da93e9efe36798866ac9d7651
mastaryan <sizemore.ryan@gmail.com>
2020-03-25 20:49:52 -0400

    Repair Method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
+const { repair, fail, succeed } = require("./enhancer.js");
 // test away!
+
+describe("enhancer.js", () => {
+  describe("repair() method", () => {
+    it("should return durability to 100", () => {
+      //arrange - setup
+      const item = {
+        durability: 3
+      };
+      const expected = {
+        durability: 100
+      };
+
+      //act - run system under test
+      const result = repair(item);
+
+      //assert - verify it works
+      expect(result).toEqual(expected);
+    });
+  });
+});
